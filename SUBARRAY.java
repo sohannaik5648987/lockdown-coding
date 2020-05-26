519001d637961f351d9e13ed269afec1a3805c2f
sohannaik5648987 <53473526+sohannaik5648987@users.noreply.github.com>
2020-05-26 17:45:50 +0530

    Create SUBARRAY.java

---
+++ SUBARRAY.java
@@ -0,0 +1,36 @@
+public class Main { 
+
+	
+	public static void cntArray(int A[], int N) 
+	{ 
+		
+	
+		int result = 0; 
+
+		for (int i = 0; i < N; i++) { 
+
+			
+			result++; 
+
+
+			int current_value = A[i]; 
+
+			for (int j = i + 1; j < N; j++) { 
+
+				
+				if (A[j] == current_value) { 
+					result++; 
+				} 
+			} 
+		} 
+
+		System.out.println(result); 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		int[] A = { 1, 2, 1, 5, 2 }; 
+		int N = A.length; 
+		cntArray(A, N); 
+	} 
+} 
