3a7c230f492d274c7497cbd6fc52d00bd03045e8
sohannaik5648987 <53473526+sohannaik5648987@users.noreply.github.com>
2020-06-03 19:36:09 +0530

    Create Objectref.java

---
+++ Objectref.java
@@ -0,0 +1,22 @@
+package object;
+
+public class Swapping {
+
+	
+		   public static void main(String[] args){
+		      int a = 30;
+		      int b = 45;
+		      System.out.println("Before swapping, a = " + a + " and b = " + b);
+		      
+		      swapFunction(a, b);
+		      
+		   }
+		   public static void swapFunction(int a, int b) {
+		     
+		      int c = a;
+		      a = b;
+		      b = c;
+		      System.out.println("After swapping(Inside), a = " + a + " b = " + b);
+		   }
+		
+}
