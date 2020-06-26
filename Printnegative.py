6bea275f51cac9f2325dd49daa7a1a5fb6fbdb72
sohannaik5648987 <53473526+sohannaik5648987@users.noreply.github.com>
2020-06-26 14:04:41 +0530

    Create Printnegative.py

---
+++ Printnegative.py
@@ -0,0 +1,10 @@
+# Python program to print negative Numbers in given range 
+  
+start = int(input("Enter the start of range: ")) 
+end = int(input("Enter the end of range: ")) 
+  
+for num in range(start, end + 1): 
+      
+    
+    if num < 0: 
+        print(num, end = " ") 
